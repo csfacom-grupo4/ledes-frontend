e54ab3d8f2a6b2f468c4aefc298b93f731e2f996
guilorentz <go.lorentz@gmail>
2023-09-19 23:56:36 -0400

    creating footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, NavbarComponent],
+  declarations: [AppComponent, HeaderComponent, NavbarComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/shared/constants/links.ts
@@ -0,0 +1,18 @@
+export const LINKS = [
+  {
+    name: 'Facebook',
+    url: 'https://www.facebook.com/ledesufms/',
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/company/ledes/',
+  },
+  {
+    name: 'FACOM',
+    url: 'https://www.facom.ufms.br/',
+  },
+  {
+    name: 'UFMS',
+    url: 'https://www.ufms.br/',
+  },
+];
