adcd44faafb4009e4e13e9bd2be553a4819b5519
guilorentz <go.lorentz@gmail>
2023-09-22 00:22:55 -0400

    adjusting responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { MemberCardComponent } from './shared/components/member-card/member-card.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, NavbarComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, NavbarComponent, FooterComponent, MemberCardComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
