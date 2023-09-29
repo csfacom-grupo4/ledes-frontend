28dbf1dd03092cb7e8bbfdb8c5a8cc35bab92c8f
TiagoMenezesVarmassera <tiagovarmassera@gmail.com>
2023-09-28 22:06:11 -0400

    telacadastrousuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,10 +8,10 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { MemberCardComponent } from './shared/components/member-card/member-card.component';
-
+import { CadastroUsuarioComponent } from './shared/components/cadastro-usuario/cadastro-usuario.component';
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, NavbarComponent, FooterComponent, MemberCardComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HeaderComponent, NavbarComponent, FooterComponent, MemberCardComponent, CadastroUsuarioComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
