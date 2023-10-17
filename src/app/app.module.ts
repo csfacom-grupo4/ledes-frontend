472a29e3029455b8967389e92e9179ab2891df94
guilorentz <go.lorentz@gmail>
2023-10-16 21:28:13 -0400

    adjusting login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -30,7 +30,7 @@ import { GerenciarPapelVinculoComponent } from './features/gerenciar-papel-vincu
     FormUnitarioPapelVinculoComponent,
     GerenciarPapelVinculoComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
