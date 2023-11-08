1168148424e1b5a76246810c329e5d08ef47b3b5
TiagoMenezesVarmassera <tiagovarmassera@gmail.com>
2023-11-07 22:43:43 -0400

    atualizalista-projetos, view projeto, member card

--- src/app/general/components/projetos/lista-projetos/lista-projetos.component.ts
+++ src/app/general/components/projetos/lista-projetos/lista-projetos.component.ts
@@ -13,10 +13,10 @@ export class ListaProjetosComponent {
   constructor() {
     // Simulação de dados da API (substitua isso com a lógica de chamada real da API)
     this.listaProjetos = [
-      { id: 1, titulo: 'Projeto de desenvolvimento de software sobre algum tema', coordenador: 'Prof. Ricardo Kondo', status: 'EM ANDAMENTO', tipo: 'EXTENSÃO', imagem: '', dataInicio: '01/01/2023' },
-      { id: 2, titulo: 'Projeto de desenvolvimento de software sobre algum tema', coordenador: 'Prof. Ricardo Kondo', status: 'CONCLUÍDO', tipo: 'EXTENSÃO',imagem: '', dataInicio: '02/01/2023' },
-      { id: 3, titulo: 'Projeto de desenvolvimento de software sobre algum tema', coordenador: 'Prof. Ricardo Kondo', status: 'CONCLUÍDO', tipo: 'EXTENSÃO',imagem: '', dataInicio: '02/01/2023' },
-      { id: 4, titulo: 'Projeto de desenvolvimento de software sobre algum tema', coordenador: 'Prof. Ricardo Kondo', status: 'DESCONTINUADO', tipo: 'EXTENSÃO',imagem: '', dataInicio: '02/01/2023' },
+      { id: 1, titulo: 'Projeto de desenvolvimento de software sobre algum tema', data_inicio: '01/01/2023', data_termino: null , coordenador: 'Prof. Ricardo Kondo', situacao: 'EM ANDAMENTO', tipo: 'EXTENSÃO', imagem: ''},
+      { id: 2, titulo: 'Projeto de desenvolvimento de software sobre algum tema', data_inicio: '02/01/2023', data_termino: '01/03/2023', coordenador: 'Prof. Ricardo Kondo', situacao: 'CONCLUÍDO', tipo: 'EXTENSÃO',imagem: '' },
+      { id: 3, titulo: 'Projeto de desenvolvimento de software sobre algum tema', data_inicio: '02/01/2023', data_termino: '01/05/2023', coordenador: 'Prof. Ricardo Kondo', situacao: 'CONCLUÍDO', tipo: 'EXTENSÃO',imagem: ''},
+      { id: 4, titulo: 'Projeto de desenvolvimento de software sobre algum tema', data_inicio: '02/01/2023', data_termino: '01/04/2023', coordenador: 'Prof. Ricardo Kondo', situacao: 'DESCONTINUADO', tipo: 'EXTENSÃO',imagem: '' },
       // Adicione mais projetos conforme necessário
     ];
   }
