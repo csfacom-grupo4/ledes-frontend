a4474f18b0614e2374a1ff9760ce08f4c0449b35
guilorentz <go.lorentz@gmail>
2023-11-24 19:44:24 -0400

    Add cadastro noticia

--- src/app/management/components/noticias/cadastro-noticia/cadastro-noticia.component.ts
+++ src/app/management/components/noticias/cadastro-noticia/cadastro-noticia.component.ts
@@ -1,10 +1,91 @@
 import { Component } from '@angular/core';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
 
 @Component({
   selector: 'app-cadastro-noticia',
   templateUrl: './cadastro-noticia.component.html',
-  styleUrls: ['./cadastro-noticia.component.scss']
+  styleUrls: ['./cadastro-noticia.component.scss'],
 })
 export class CadastroNoticiaComponent {
+  conteudo: string = ''; // Conteúdo do editor
 
+  htmlContent = this.conteudo;
+  editorConfig: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '15rem',
+    minHeight: '15rem',
+    maxHeight: 'auto',
+    width: 'auto',
+    minWidth: '0',
+    translate: 'no',
+    enableToolbar: true,
+    showToolbar: true,
+    placeholder: 'Insira o texto...',
+    defaultParagraphSeparator: 'p',
+    defaultFontName: 'Arial',
+    defaultFontSize: '',
+    fonts: [
+      { class: 'arial', name: 'Arial' },
+      { class: 'times-new-roman', name: 'Times New Roman' },
+      { class: 'calibri', name: 'Calibri' },
+      { class: 'comic-sans-ms', name: 'Comic Sans MS' },
+    ],
+    /* customClasses: [
+      {
+        name: 'quote',
+        class: 'quote',
+      },
+      {
+        name: 'redText',
+        class: 'redText'
+      },
+      {
+        name: 'titleText',
+        class: 'titleText',
+        tag: 'h1',
+      },
+    ],*/
+    uploadUrl: 'v1/image',
+    /* upload: (file: File) => {
+      // Implemente a lógica para enviar o arquivo para o servidor aqui
+      // Por exemplo, você pode usar um serviço HTTP para enviar o arquivo para uma rota específica no backend
+      this.seuServicoHttp.enviarArquivoParaServidor(file).subscribe(
+        response => {
+          console.log('Arquivo enviado com sucesso:', response);
+          // Faça algo com a resposta do servidor, se necessário
+        },
+        error => {
+          console.error('Erro ao enviar arquivo:', error);
+          // Lide com o erro, se necessário
+        }
+      );
+    },*/
+    uploadWithCredentials: false,
+    sanitize: true,
+    toolbarPosition: 'top',
+    toolbarHiddenButtons: [['bold', 'italic'], ['fontSize']],
+  };
+
+  imagemProjeto: string | ArrayBuffer | null = null;
+
+  membros = [
+    'Tony Stark',
+    'Steve Rogers',
+    'Thor Odinson',
+    'Bruce Banner',
+    'Natasha Romanoff',
+    'Clint Barton',
+  ];
+
+  onFileSelected(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagemProjeto = reader.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 }
--- src/app/management/components/noticias/lista-noticias/lista-noticias.component.ts
+++ src/app/management/components/noticias/lista-noticias/lista-noticias.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lista-noticias',
   templateUrl: './lista-noticias.component.html',
-  styleUrls: ['./lista-noticias.component.scss']
+  styleUrls: ['./lista-noticias.component.scss'],
 })
 export class ListaNoticiasComponent {
+  projetos = [
+    'Projeto Armadura Dourada',
+    'Projeto Infinito',
+    'Projeto O Massacre',
+    'Projeto Planeta Hulk',
+    'Projeto Guerra Civil',
+    'Projeto A Queda de Murdock',
+    'Projeto Guerra Infinita',
+    'Projeto Guerra Secreta',
+    'Projeto Reinado Sombrio',
+    'Projeto Dinastia M',
+    'Projeto Cerco',
+    'Projeto A Noite Mais Densa',
+    'Projeto Aniquilação',
+    'Projeto Império Secreto',
+    'Projeto Reino do Amanhã',
+    'Projeto A Era de Apocalipse',
+    'Projeto Guerra Kree-Skrull',
+    'Projeto Operação: Insurgência',
+    'Projeto Planeta Solitário',
+    'Projeto Dizimação',
+  ];
 
+  constructor(private router: Router) {}
+
+  navigate() {
+    this.router.navigate(['management/cadastrar-noticia']);
+  }
 }
